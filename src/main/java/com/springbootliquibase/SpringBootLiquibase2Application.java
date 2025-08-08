00e0ba7a6605204dc571c1ff57251a38a8481ed0
Vinod Kumar <vinodkumar@192.168.1.4>
2025-08-08 12:47:38 +0530

    Initial Commit

---
+++ src/main/java/com/springbootliquibase/SpringBootLiquibase2Application.java
@@ -0,0 +1,13 @@
+package com.springbootliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiquibase2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLiquibase2Application.class, args);
+	}
+
+}
